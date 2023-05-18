3427e6b5aff47235845903676b42be65f025c19a
Dhouib Mohamed <mohamed.dhouib@insat.ucar.tn>
2023-05-18 15:36:48 +0100

    code

--- src/model.py
+++ src/model.py
@@ -50,59 +50,61 @@ def logisticRegressionSummary(model, column_names):
         classes = model.classes_
 
     # Create a plot for each class
-    for i,c in enumerate(classes):
+    for i, c in enumerate(classes):
         # Plot the coefficients as bars
-        fig = plt.figure(figsize=(8,len(column_names)/3))
+        fig = plt.figure(figsize=(8, len(column_names) / 3))
         fig.suptitle('Logistic Regression Coefficients for Class ' + str(c), fontsize=16)
-        rects = plt.barh(column_names, model.coef_[i],color="lightblue")
+        rects = plt.barh(column_names, model.coef_[i], color="lightblue")
 
         # Annotate the bars with the coefficient values
         for rect in rects:
-            width = round(rect.get_width(),4)
+            width = round(rect.get_width(), 4)
             plt.gca().annotate('  {}  '.format(width),
                                xy=(0, rect.get_y()),
-                               xytext=(0,2),
+                               xytext=(0, 2),
                                textcoords="offset points",
-                               ha='left' if width<0 else 'right', va='bottom')
+                               ha='left' if width < 0 else 'right', va='bottom')
         plt.show()
-        #for pair in zip(X.columns, model_lr.coef_[i]):
+        # for pair in zip(X.columns, model_lr.coef_[i]):
         #    print (pair)
 
+
 def decisionTreeSummary(model, column_names):
-    '''Show a summary of the trained decision tree model'''
+    """Show a summary of the trained decision tree model"""
 
     # Plot the feature importances as bars
-    fig = plt.figure(figsize=(8,len(column_names)/3))
+    fig = plt.figure(figsize=(8, len(column_names) / 3))
     fig.suptitle('Decision tree feature importance', fontsize=16)
-    rects = plt.barh(column_names, model.feature_importances_,color="khaki")
+    rects = plt.barh(column_names, model.feature_importances_, color="khaki")
 
     # Annotate the bars with the feature importance values
     for rect in rects:
-        width = round(rect.get_width(),4)
+        width = round(rect.get_width(), 4)
         plt.gca().annotate('  {}  '.format(width),
                            xy=(width, rect.get_y()),
-                           xytext=(0,2),
+                           xytext=(0, 2),
                            textcoords="offset points",
                            ha='left', va='bottom')
 
     plt.show()
 
+
 def linearRegressionSummary(model, column_names):
     '''Show a summary of the trained linear regression model'''
 
     # Plot the coeffients as bars
-    fig = plt.figure(figsize=(8,len(column_names)/3))
+    fig = plt.figure(figsize=(8, len(column_names) / 3))
     fig.suptitle('Linear Regression Coefficients', fontsize=16)
-    rects = plt.barh(column_names, model.coef_,color="lightblue")
+    rects = plt.barh(column_names, model.coef_, color="lightblue")
 
     # Annotate the bars with the coefficient values
     for rect in rects:
-        width = round(rect.get_width(),4)
+        width = round(rect.get_width(), 4)
         plt.gca().annotate('  {}  '.format(width),
                            xy=(0, rect.get_y()),
-                           xytext=(0,2),
+                           xytext=(0, 2),
                            textcoords="offset points",
-                           ha='left' if width<0 else 'right', va='bottom')
+                           ha='left' if width < 0 else 'right', va='bottom')
     plt.show()
 
 
@@ -119,7 +121,7 @@ def viewDecisionTree(model, column_names):
 
 
 def find_outliers(feature):
-    '''Return a list of outliers in the data'''
+    """Return a list of outliers in the data"""
 
     # Temporarily replace nulls with mean so they don't cause an error
     feature = feature.fillna(feature.mean())
